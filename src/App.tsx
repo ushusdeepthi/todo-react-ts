a7fb10e3e2ab6f738195e9bc58dcba6355b32bd1
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-11-22 19:29:01 +0100

    :tada: Initialise project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const App: React.FC =()=>{}- can declare a function explixity defining the type as FC ie Fubnction component
+// function App() {
+//   return (
+//     <div className="App">
+//     </div>
+//   );
+// }
+const App: React.FC =()=>{
+  return <div className="App"> </div>
 }
+
 
 export default App;
